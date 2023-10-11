39a1c1466a329bf7fe3fde94287285061efc4b5e
sofymiranda <mirandaspfya@gmail.com>
2023-10-11 11:45:40 -0300

    atividade 5

---
+++ atv5.py
@@ -0,0 +1,8 @@
+# Digite uma metragem e veja isso em cm e mm:
+print ("Digite uma metragem e veja isso em cm e mm:")
+numero= float (input("digite a metragem:"))
+centimetro= numero * 100
+milimetro= numero * 1000
+print( f"em centimetros {centimetro} e milimetros {milimetro}")
+
+
